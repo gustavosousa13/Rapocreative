e9aaab47da487f1f77769944ab0a2488e9c05393
Luis Gustavo de sousa <dgutarr2@gmail.com>
2024-06-16 17:41:16 -0300

    inclusão de imagem no componente subscribe

--- src/Components/Body/Subscribe.jsx
+++ src/Components/Body/Subscribe.jsx
@@ -6,88 +6,115 @@ import { Typography } from '@mui/material';
 
 export default function Subscribe() {
   return (
-    <Box sx={{borderTop:'solid 1px' , flexDirection: 'row', height:'65vh', marginLeft:'50px', marginRight:'50px', fontFamily: 'Poppins, sans-serif',}}>
+    <Box sx={{ borderTop: 'solid 1px', flexDirection: 'row', height: '65vh', marginLeft: '50px', marginRight: '50px', fontFamily: 'Poppins, sans-serif', position: 'relative' }}>
     
-    
-    <Box
-      sx={{
-        width: '50%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        paddingLeft: '50px',
-        paddingTop:'100px'
-      }}
-    >
-      <Typography
-        variant="h4"
-        sx={{ textAlign: 'left', marginBottom: '20px', marginRight: '30px',marginLeft:'-35px', fontFamily:'poppins' }}
-      >
-        Cadastre-se para mais informações.
-      </Typography>
-      <Typography
-        variant="body1"
-        sx={{ textAlign: 'left', marginRight: '140px',marginLeft:'-35px', fontFamily:'poppins' }}
-      >
-       Deixe aqui seu contato, para receber nossas novidades e para entrar em contato para mais informações.
-      </Typography>
-    </Box>
-     
-    
-    <Box
-      component="form"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        marginLeft: '650px', 
-        marginTop: '-220px',
-        height:'350px',
-        marginRight:'-25px'
+      <Box
+        component="img"
+        src="assets/cachoeira.png"
+        alt="cachoeira"
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          height: 'auto',
+          width: '750px',
+          marginLeft: '-70px',
+          zIndex: 1,
+          // Ajustes responsivos
+          '@media (max-width: 1200px)': {
+            height: '250px',
+            width: '250px',
+          },
+          '@media (max-width: 768px)': {
+            height: '200px',
+            width: '200px',
+          },
+          '@media (max-width: 480px)': {
+            height: '150px',
+            width: '150px',
+          },
         }}
-      noValidate
-      autoComplete="off"
-    >
-      <Box sx={{marginRight:'-100px'}}>
-      <Grid container spacing={2} >
-        <Grid item xs={8} sm={6}>
-          <TextField
-            id="outlined-multiline-flexible"
-            label="Nome completo"
-            placeholder="Digite seu nome completo"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={8} sm={4}>
-          <TextField
-            id="outlined-textarea"
-            label="Telefone"
-            placeholder="Digite telefone com DDD"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={6} sm={10}>
-          <TextField
-            id="outlined-textarea"
-            label="Email"
-            placeholder="Digite seu email"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12} sm={10} >
-          <TextField
-            id="outlined-multiline-static"
-            label="Digite sua mensagem"
-            placeholder="Deixe aqui seu recado, que entraremos em contato!"
-            multiline
-            rows={6}
-            fullWidth
-           
-          />
-        </Grid>
-      </Grid>
+      />
+      <Box
+        sx={{
+          position: 'relative',
+          width: '50%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'flex-start',
+          paddingLeft: '190px',
+          paddingTop: '50px',
+          zIndex: 2,
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{ textAlign: 'left', marginBottom: '20px', marginRight: '0px', marginLeft: '0px', fontFamily: 'Poppins', fontWeight:'bold', fontSize:'44px' }}
+        >
+            Fale com a gente!
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ textAlign: 'left', marginRight: '-20pxpx', marginLeft: '0px', fontFamily: 'Poppins' }}
+        >
+          Deixe aqui sua mensagem. Além de nos contatar, você receberá notificações de lançamentos.
+        </Typography>
       </Box>
-    </Box>
+      
+      <Box
+        component="form"
+        sx={{
+          position: 'absolute',
+          top: '50px', 
+          left: '700px',
+          display: 'flex',
+          flexDirection: 'column',
+          height: '350px',
+          zIndex: 2,
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Box sx={{ marginRight: '-100px' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={8} sm={6}>
+              <TextField
+                id="outlined-multiline-flexible"
+                label="Nome completo"
+                placeholder="Digite seu nome completo"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={8} sm={4}>
+              <TextField
+                id="outlined-textarea"
+                label="Telefone"
+                placeholder="Digite telefone com DDD"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6} sm={10}>
+              <TextField
+                id="outlined-textarea"
+                label="Email"
+                placeholder="Digite seu email"
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={12} sm={10}>
+              <TextField
+                id="outlined-multiline-static"
+                label="Digite sua mensagem"
+                placeholder="Deixe aqui seu recado, que entraremos em contato!"
+                multiline
+                rows={6}
+                fullWidth
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </Box>
   );
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Container, Grid, Typography,Link,IconButton, Stack,} from '@mui/material';
+import { Box, Container, Grid, Typography, Link, IconButton, Stack } from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import YouTubeIcon from '@mui/icons-material/YouTube';
@@ -8,21 +8,25 @@ import GridImage from './Grid-image.jsx';
 
 const Footer = () => {
   return (
-    <Box sx={{ backgroundColor: '#F2F1E4', py: 6, borderTop:'solid 1px', height:'70vh'}}>
+    <Box sx={{ backgroundColor: '#F2F1E4', py: 6, borderTop: 'solid 1px', height: '70vh' }}>
       <Container maxWidth="xl" sx={{ px: 0 }}>
         <Grid container spacing={5}>
-          <Grid item xs={12} sm={4} md={3}>
-            <Box component="img"
-                src="assets/logo-rapo.png"
-                alt="Logo"
-                sx={{ width: '270px', height: 'auto', marginLeft:'0px' }} 
+          <Grid item xs={12} sm={4} md={3} lg={3}>
+            <Box
+              component="img"
+              src="assets/logo-rapo.png"
+              alt="Logo"
+              sx={{ width: '270px', height: 'auto', marginLeft: '0px', marginLeft:{xs:7, sm:1}}}
             />
-            
-            <Typography variant="body1" sx={{fontSize:'14px', letterSpacing: 0, fontFamily: 'Poppins, sans-serif', marginLeft:'30px', lineHeight:'px' }}>
-            Utilizamos técnicas inovadoras para produzir estampas vibrantes e cheias de personalidade. 
-            Seja para uma coleção completa ou uma peça única.
+
+            <Typography
+              variant="body1"
+              sx={{ fontSize: '14px', letterSpacing: 0, fontFamily: 'Poppins, sans-serif', marginLeft: '30px', lineHeight: 'px' }}
+            >
+              Utilizamos técnicas inovadoras para produzir estampas vibrantes e cheias de personalidade. Seja para uma coleção
+              completa ou uma peça única.
             </Typography>
-            <Stack direction="row" spacing={2} sx={{ mt: 2, marginLeft:'20px' }}>
+            <Stack direction="row" spacing={2} sx={{ mt: 2, marginLeft: '20px' }}>
               <IconButton href="#" aria-label="Instagram">
                 <InstagramIcon />
               </IconButton>
@@ -30,7 +34,6 @@ const Footer = () => {
                 <YouTubeIcon />
               </IconButton>
               <IconButton href="#" aria-label="TikTok">
-                {/* Substitua por um ícone de TikTok correto se disponível */}
                 <FacebookIcon />
               </IconButton>
               <IconButton href="#" aria-label="Pinterest">
@@ -38,106 +41,109 @@ const Footer = () => {
               </IconButton>
             </Stack>
           </Grid>
-          <Grid item xs={12} sm={4} md={3}>
-      <Stack sx={{ gap: '15px', fontFamily: 'Poppins, sans-serif', color: 'black', marginTop:'80px', marginLeft:'130px' }}>
-        <Grid container spacing={8}>
-          <Grid item xs={6}>
-            <Link href="#" underline="none">
-              <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding:'10px'}}>
-                HOME
-              </Typography>
-            </Link>
-            <Link href="#" underline="none">
-              <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding:'10px' }}>
-                SERVICES
-              </Typography>
-            </Link>
-            <Link href="#" underline="none">
-              <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding:'10px' }}>
-                ABOUT
-              </Typography>
-            </Link>
+          <Grid item xs={8} sm={4} md={3}>
+            <Stack sx={{ gap: '15px', fontFamily: 'Poppins, sans-serif', color: 'black', marginTop: '40px', marginLeft: '130px' }}>
+              <Grid container spacing={0}>
+                <Grid item xs={8}>
+                  <Link href="#" underline="none">
+                    <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding: '10px' }}>
+                      HOME
+                    </Typography>
+                  </Link>
+                  <Link href="#" underline="none">
+                    <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding: '10px' }}>
+                      SERVICES
+                    </Typography>
+                  </Link>
+                  <Link href="#" underline="none">
+                    <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding: '10px' }}>
+                      ABOUT
+                    </Typography>
+                  </Link>
+                </Grid>
+                <Grid item xs={6}>
+                  <Link href="#" underline="none">
+                    <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding: '10px' }}>
+                      CONTACT
+                    </Typography>
+                  </Link>
+                  <Link href="#" underline="none">
+                    <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding: '10px' }}>
+                      BLOG
+                    </Typography>
+                  </Link>
+                  <Link href="#" underline="none">
+                    <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding: '10px' }}>
+                      PROGRAM
+                    </Typography>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Stack>
           </Grid>
-          <Grid item xs={6}>
-            <Link href="#" underline="none">
-              <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding:'10px' }}>
-                CONTACT
-              </Typography>
-            </Link>
-            <Link href="#" underline="none">
-              <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding:'10px' }}>
-                BLOG
-              </Typography>
-            </Link>
-            <Link href="#" underline="none">
-              <Typography variant="body1" sx={{ letterSpacing: 0, color: 'black', padding:'10px' }}>
-                PROGRAM
-              </Typography>
-            </Link>
-          </Grid>
-        </Grid>
-      </Stack>
-    </Grid>
           <Grid item xs={12} sm={4} md={6}>
-            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' , marginRight:'-250px'}}>
-              <GridImage/>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%', marginRight: '-250px' }}>
+              <GridImage />
             </Box>
           </Grid>
         </Grid>
         <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: '100%',
-        padding: '10px 0',
-      }}
-    >
-      <Box
-        component="img"
-        sx={{
-          width: 450,
-          height: 80,
-          marginLeft: -5,
-        }}
-        alt="Imagem rodape"
-        src="assets/rodape2.png"
-      />
-      <Box
-        sx={{
-          flex: 1,
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <Typography
-          variant="body3"
-          textAlign="center"
           sx={{
-            letterSpacing: 1,
-            fontFamily: 'Poppins, sans-serif',
-            whiteSpace: 'nowrap', 
-            paddingLeft:15,
-            paddingRight:15,
-            width:10,
-            fontSize:15
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: '100%',
+            padding: '10px 0px',
+            
           }}
         >
-          &copy; 2024 Rapó Creative
-        </Typography>
-      </Box>
-      <Box
-        component="img"
-        sx={{
-          width: 450,
-          height: 80,
-          marginLeft: 30,
-          marginRight:0,
-        }}
-        alt="Imagem rodape"
-        src="assets/rodape1.png"
-      />
-    </Box>
+          <Box
+            component="img"
+            sx={{
+              width: 450,
+              height: 80,
+              marginLeft: -8,
+              display: { xs: 'none', sm: 'block' }, // Hide on small screens
+            }}
+            alt="Imagem rodape"
+            src="assets/rodape2.png"
+          />
+          <Box
+            sx={{
+              flex: 1,
+              display: 'flex',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography
+              variant="body3"
+              textAlign="center"
+              sx={{
+                letterSpacing: 1,
+                fontFamily: 'Poppins, sans-serif',
+                whiteSpace: 'nowrap',
+                paddingLeft: 15,
+                paddingRight: 15,
+                width: 10,
+                fontSize: 15,
+              }}
+            >
+              &copy; 2024 Rapó Creative
+            </Typography>
+          </Box>
+          <Box
+            component="img"
+            sx={{
+              width: 450,
+              height: 80,
+              marginLeft: 30,
+              marginRight: 0,
+              display: { xs: 'none', sm: 'block' }, // Hide on small screens
+            }}
+            alt="Imagem rodape"
+            src="assets/rodape1.png"
+          />
+        </Box>
       </Container>
     </Box>
   );
